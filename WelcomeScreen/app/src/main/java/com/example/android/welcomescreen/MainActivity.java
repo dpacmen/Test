8cdc49b36f01b129357ad4f341bbd97c18a8fd6a
Deepak <Deepak>
2017-06-14 22:41:25 +0530

    Firebase database

--- WelcomeScreen/app/src/main/java/com/example/android/welcomescreen/MainActivity.java
+++ WelcomeScreen/app/src/main/java/com/example/android/welcomescreen/MainActivity.java
@@ -2,12 +2,43 @@ package com.example.android.welcomescreen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView MsgTxt;
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference mRootRef = firebaseDatabase.getReference();
+    private DatabaseReference mChildRef = mRootRef.child("message");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MsgTxt = (TextView) findViewById(R.id.textview);
+        MsgTxt.setText("MESSAGE WILL APPEAR HERE.....");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mChildRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String message = dataSnapshot.getValue(String.class);
+                MsgTxt.setText(message);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
